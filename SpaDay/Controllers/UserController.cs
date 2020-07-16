640f58d84fed9409d6a48d44bad9fcf24cbed003
rhagen87 <reynold.hagen@gmail.com>
2020-07-16 15:09:15 -0500

    not working yet

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/user/add")]
+        public IActionResult Add()
+        {
+            return View("Add");
+        }
+
+        [HttpPost]
+        [Route("/user/add")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (verify == newUser.Password)
+            {
+                ViewBag.Username = newUser.Username;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.Username = newUser.Username;
+                ViewBag.Email = newUser.Email;
+                ViewBag.Error = "Please enter matching passwords.";
+                return View("Add");
+            }
+        }
+    }
+}
